8d972693a1fd1619511f66e474b95a2788e4f315
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-10-22 18:26:08 -0300

    Fix linter issues

--- enumerable.rb
+++ enumerable.rb
@@ -3,20 +3,20 @@
 
 module Enumerable
   def my_each
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     length.times { |i| yield(self[i]) }
     self
   end
 
   def my_each_with_index
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     length.times { |i| yield(self[i], i) }
   end
 
   def my_select
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     arr = []
     my_each { |i| yield(i) ? arr.push(i) : i }
@@ -24,7 +24,7 @@ module Enumerable
   end
 
   def my_all?
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     my_each do |i|
       case yield i
@@ -36,7 +36,7 @@ module Enumerable
   end
 
   def my_any?
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     my_each do |i|
       case yield i
@@ -48,19 +48,19 @@ module Enumerable
   end
 
   def my_none?
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     (my_any? { |i| yield(i) == true }) != true
   end
 
   def my_count
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     my_each_with_index { |_n, i| i + 1 }
   end
 
   def my_map(proc = nil)
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
     
     arr = []
     if proc
@@ -72,7 +72,7 @@ module Enumerable
   end
 
   def my_inject
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     x = shift
     y = x
@@ -82,7 +82,7 @@ module Enumerable
   end
 
   def multiply_els(arr)
-    return self.to_enum unless block_given?
+    return .to_enum unless block_given?
 
     arr.my_inject { |x, num| x *= num }
   end
